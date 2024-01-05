71bddf34ad156bff5549f3a17f10540aef1d99c5
atrox3d <atrox3d@gmail.com>
2024-01-05 13:33:50 +0100

    add init

--- atrox3d/gitfiles/gitfiles.py
+++ atrox3d/gitfiles/gitfiles.py
@@ -1,22 +1,29 @@
 import os
+import shutil
+from pathlib import Path
+
 from abc import ABC, abstractmethod
 
 from ..helpers.logger import get_logger
 
-logger = get_logger(__name__)
+logger = get_logger(__name__, 'DEBUG')
 
 workdir = os.getcwd()
 moduledir = os.path.dirname(__file__)
 
 class GitFile(ABC):
-    def __init__(self, source=moduledir, dest=workdir) -> None:
-        self.source = source
-        self.dest = dest
+    def __init__(self, sourcedir=moduledir, destdir=workdir) -> None:
+        self.sourcedir = Path(sourcedir)
+        self.destdir = Path(destdir)
+        logger.debug(f'{self.sourcedir = }')
+        logger.debug(f'{self.destdir = }')
 
 class GitIgnore(GitFile):
     def __init__(self, source=moduledir, dest=workdir) -> None:
         super().__init__(source, dest)
-        print(f'{self.source = }')
-        print(f'{self.dest = }')
-
-
+        self.filename = '.gitignore'
+        self.source = self.sourcedir / self.filename
+        self.dest = self.destdir / self.filename
+        logger.debug(f'{self.source = }')
+        logger.debug(f'{self.dest = }')
+    
