beb57acfd37d3be23e9c5b95a4ff4f5914b621ac
atrox3d <atrox3d@gmail.com>
2024-01-06 10:20:18 +0100

    add automatic properties

--- atrox3d/helpers/loggersettings.py
+++ atrox3d/helpers/loggersettings.py
@@ -15,21 +15,28 @@ asctime = LogConfigItem('asctime', format='%Y/%m/%d %H:%M:%S')
 module = LogConfigItem('module', width=12)
 function = LogConfigItem('funcName', width=12)
 level = LogConfigItem('levelname', width=len('CRITICAL'))
+message = LogConfigItem('message')
 
-@dataclass
-class LogConfig:
-    items: list[LogConfigItem]
-    separator: str = ' | '
+class LogFormat:
+    def __init__(self, *items: list[LogConfigItem], separator=' | ') -> None:
+        self._items = list(items)
+        for item in items:
+            setattr(self, item.name, item)
+        self._separator = separator
 
     def __str__(self) -> str:
-        return self.separator.join(self.items)
+        # return self._separator.join([str(item) for item in self._items])
+        items = [str(item) for name, item in vars(self).items() if not name.startswith('_')]
+        return self._separator.join(items)
 
-default_config = LogConfig(asctime, module)
+default_config = LogFormat(asctime, module, function, level, message)
 
 def main():
-    for item in [asctime, module, level]:
+    for item in [asctime, module, level, message]:
         print(repr(item))
         print(item)
 
+    default_config.asctime.width = 15
+    print(f'{default_config}')
 if __name__ == '__main__':
     main()
