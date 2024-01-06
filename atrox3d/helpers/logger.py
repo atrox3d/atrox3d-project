39070798e15b7856a27f8c8cf806d47ae48c8c6b
atrox3d <atrox3d@gmail.com>
2024-01-06 06:34:13 +0100

    add kwargs

--- atrox3d/helpers/logger.py
+++ atrox3d/helpers/logger.py
@@ -1,26 +1,48 @@
 import logging
 import sys
 
+class CONST:
+    pass
 
-def get_logger(name, level=logging.INFO):
-    """
-    get standard logger to stdout, level INFO
-    :param name:
-    :param level:
-    :return:
-    """
-    modulename_width = 12
-    funcname_width = 12
-    levelname_width = str(len("CRITICAL"))
+CONST.DATE_FORMAT = '%Y/%m/%d %H:%M:%S'
+CONST.ASCTIME_WIDTH = len(CONST.DATE_FORMAT)
+CONST.MODULENAME_WIDTH = 12
+CONST.FUNCNAME_WIDTH = 12
+CONST.LEVELNAME_WIDTH = str(len("CRITICAL"))
+# CONST.LINE_FORMAT = (
+#             f"%(asctime)s | "
+#             f"%(module){CONST.MODULENAME_WIDTH}s.py | "
+#             f"%(funcName){CONST.FUNCNAME_WIDTH}s() | "
+#             f"%(levelname)-{CONST.LEVELNAME_WIDTH}s | "
+#             f"%(message)s"
+#     )
 
-    line_format = (
-            f"%(asctime)s | "
-            f"%(module){modulename_width}s.py | "
-            # f"%(funcName){funcname_width}s() | "
-            f"%(levelname)-{levelname_width}s | "
-            f"%(message)s"
-    )
-    date_format = '%Y/%m/%d %H:%M:%S'
+
+def get_logger(name: str, level=logging.INFO,
+               asctime_width=CONST.ASCTIME_WIDTH,
+               modulename_width=CONST.ASCTIME_WIDTH,
+               funcname_width=CONST.FUNCNAME_WIDTH,
+               levelname_width=CONST.LEVELNAME_WIDTH,
+               lineformat=None,
+               dateformat=CONST.DATE_FORMAT,
+               ):
+    print(f'{lineformat=}')
+    print(f'{asctime_width=}')
+    print(f'{modulename_width=}')
+    print(f'{funcname_width=}')
+    print(f'{levelname_width=}')
+
+    if lineformat:
+        line_format = lineformat
+    else:
+        line_format = f"%(asctime)s | " if asctime_width else ""
+        line_format +=f"%(module){modulename_width}s.py | " if modulename_width else ""
+        line_format +=f"%(funcName){funcname_width}s() | " if funcname_width else ""
+        line_format +=f"%(levelname)-{levelname_width}s | " if levelname_width else ""
+        line_format +=f"%(message)s"
+    
+    print(f'{line_format=}')
+    date_format = dateformat or CONST.DATE_FORMAT
 
     logging.basicConfig(
         level=level,
@@ -30,3 +52,14 @@ def get_logger(name, level=logging.INFO):
     )
     logger = logging.getLogger(name)
     return logger
+
+
+if __name__ == '__main__':
+    logger = get_logger(
+                    'test',
+                    asctime_width=0, 
+                    funcname_width=0,
+                    modulename_width=0
+                )
+
+    logger.info('testing')
