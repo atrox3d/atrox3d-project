2dc18c8aa0d1f688df21c9ee1aa4f497785accea
atrox3d <atrox3d@gmail.com>
2024-01-06 08:47:07 +0100

    restart

--- atrox3d/helpers/logger.py
+++ atrox3d/helpers/logger.py
@@ -1,32 +1,39 @@
 import logging
 import sys
 
+class CONST:
+    pass
 
-def get_logger(name, level=logging.INFO):
-    """
-    get standard logger to stdout, level INFO
-    :param name:
-    :param level:
-    :return:
-    """
-    modulename_width = 12
-    funcname_width = 12
-    levelname_width = str(len("CRITICAL"))
+CONST.DATE_FORMAT = '%Y/%m/%d %H:%M:%S'
+CONST.ASCTIME_WIDTH = len(CONST.DATE_FORMAT)
+CONST.MODULENAME_WIDTH = 12
+CONST.FUNCNAME_WIDTH = 12
+CONST.LEVELNAME_WIDTH = str(len("CRITICAL"))
 
-    line_format = (
-            f"%(asctime)s | "
-            f"%(module){modulename_width}s.py | "
-            # f"%(funcName){funcname_width}s() | "
-            f"%(levelname)-{levelname_width}s | "
-            f"%(message)s"
-    )
-    date_format = '%Y/%m/%d %H:%M:%S'
+def get_logger(name: str, level=logging.INFO,
+               date_width=CONST.ASCTIME_WIDTH,
+               module_width=CONST.ASCTIME_WIDTH,
+               func_width=CONST.FUNCNAME_WIDTH,
+               level_width=CONST.LEVELNAME_WIDTH,
+               custom_format=None,
+               dateformat=CONST.DATE_FORMAT,
+               ):
+    if custom_format:
+        line_format = custom_format
+    else:
+        line_format = f"%(asctime)s | " if date_width else ""
+        line_format +=f"%(module){module_width}s.py | " if module_width else ""
+        line_format +=f"%(funcName){func_width}s() | " if func_width else ""
+        line_format +=f"%(levelname)-{level_width}s | " if level_width else ""
+        line_format +=f"%(message)s"
+        
+        date_format = dateformat
 
-    logging.basicConfig(
-        level=level,
-        format=line_format,
-        datefmt=date_format,
-        # stream=sys.stdout
-    )
+    logging.basicConfig(level=level, format=line_format, datefmt=date_format)
     logger = logging.getLogger(name)
     return logger
+
+
+if __name__ == '__main__':
+    logger = get_logger( 'test', date_width=0, func_width=0, module_width=0 )
+    logger.info('testing')
