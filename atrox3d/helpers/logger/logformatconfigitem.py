928475dc39a3a163c212ae43ebe5ea0af1e5b6fd
atrox3d <atrox3d@gmail.com>
2024-01-07 12:32:50 +0100

    refactor logger helpers to logger package

---
+++ atrox3d/helpers/logger/logformatconfigitem.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+@dataclass
+class LogFormatConfigItem:
+    name: str
+    width: int = None
+    format: str = None
+    enabled: bool = True    # remove
+
+    def __str__(self) -> str:
+        width =  self.width and str(self.width) or ''
+        return f'%({self.name}){width}s'
+
+class Constants:
+    ASCTIME_FORMAT='%Y/%m/%d %H:%M:%S'
+    MODULE_WIDTH = 12
+    FUNCNAME_WIDTH = 12
+    LEVEL_WIDTH=len('CRITICAL')
+
+class Defaults:
+    asctime = LogFormatConfigItem('asctime', format=Constants.ASCTIME_FORMAT)
+    module = LogFormatConfigItem('module', width=Constants.MODULE_WIDTH)
+    funcname = LogFormatConfigItem('funcName', width=Constants.FUNCNAME_WIDTH)
+    level = LogFormatConfigItem('levelname', width=Constants.LEVEL_WIDTH)
+    message = LogFormatConfigItem('message')
+
+    def __iter__(self):
+        for item in self._getlist():
+            yield item
+
+    def _getlist(self):
+        return [getattr(self, atr) for atr in dir(self) if not atr.startswith('_')]
+    
