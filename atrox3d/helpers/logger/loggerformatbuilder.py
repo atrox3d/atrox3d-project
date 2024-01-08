c5bace402899e17bace2e992eafccd876c4a0d40
atrox3d <atrox3d@gmail.com>
2024-01-08 11:26:02 +0100

    rename methods

--- atrox3d/helpers/logger/loggerformatbuilder.py
+++ atrox3d/helpers/logger/loggerformatbuilder.py
@@ -6,28 +6,28 @@ class LoggerFormatBuilder:
     def __init__(self) -> None:
         self.items = []
     
-    def add_item(self, item: LogFormatConfigItem):
+    def append_item(self, item: LogFormatConfigItem):
         self.items.append(item)
     
-    def add_field(self, name, width=None, fieldtype='s'):
+    def add_item(self, name, width=None, fieldtype='s'):
         item = LogFormatConfigItem(name, width, fieldtype)
-        self.add_item(item)
+        self.append_item(item)
         return self
     
     def add_asctime(self, width=None):
-        return self.add_field('asctime', width)
+        return self.add_item('asctime', width)
 
     def add_module(self, width=Constants.MODULE_WIDTH):
-        return self.add_field('module', width)
+        return self.add_item('module', width)
 
     def add_function(self, width=Constants.FUNC_WIDTH):
-        return self.add_field('funcName', width)
+        return self.add_item('funcName', width)
 
     def add_levelname(self, width=Constants.LEVEL_WIDTH):
-        return self.add_field('levelname', width)
+        return self.add_item('levelname', width)
 
     def add_message(self, width=None):
-        return self.add_field('message', width)
+        return self.add_item('message', width)
 
     def build(self) -> LoggerFormat:
         if 'message' not in [item.name for item in self.items]:
@@ -39,6 +39,6 @@ if __name__ == '__main__':
                 .add_asctime()\
                 .add_function()\
                 .add_levelname()\
-                .add_field('test')\
+                .add_item('test')\
                 .build()
     print(format)
