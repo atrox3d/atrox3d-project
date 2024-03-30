5d8edd35239923cadc2e14c06863fb02845fce2d
atrox3d <atrox3d@gmail.com>
2024-03-30 12:21:43 +0100

    manage path attribute as string or Path with get_path()

--- atrox3d/simplegit/repo.py
+++ atrox3d/simplegit/repo.py
@@ -2,16 +2,26 @@
 # from modules.simplegit.status import GitStatus
 
 
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from pathlib import Path
 
 
 @dataclass
 class GitRepo:
     name: str
-    path: Path
+    path: str
     remote: str = None
 
+
+    def __post_init__(self):
+        self.path = str(self.path)
+    
+    def asdict(self):
+        return(asdict(self))
+    
+    def get_path(self):
+        return Path(self.path)
+    
     # def get_status(self) -> GitStatus:
     #     return get_status(self)
 
