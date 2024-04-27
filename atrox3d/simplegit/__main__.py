6016475b6132a6e4a7df55319dee8033b031130a
atrox3d <atrox3d@gmail.com>
2024-04-27 11:28:40 +0200

    add logging to __main__.py

--- atrox3d/simplegit/__main__.py
+++ atrox3d/simplegit/__main__.py
@@ -1,20 +1,32 @@
 import os
 import sys
+import logging
+from pathlib import Path
 
 from . import _options
 from . import git
 
+logger = logging.getLogger(__name__)
+
 def main():
+    LOGFILE = str(Path(__file__).parent / Path(__file__).stem) + '.log'
+    handlers = [
+        logging.FileHandler(LOGFILE, mode='w'),
+        logging.StreamHandler()
+    ]
+
+    logging.basicConfig(level='INFO', format='%(levelname)s | %(message)s', handlers=handlers)
     args = _options.parse()
 
-    print(f'{args = }')
-    print(f'{os.getcwd()}')
+    logger.info(f'{LOGFILE = }')
+    logger.info(f'{args = }')
+    logger.info(f'cwd = {os.getcwd()}')
 
     try:
         repo = git.get_repo(os.getcwd())
         
         current_branch = git.get_current_branch(repo)
-        print(f'{current_branch = }')
+        logger.info(f'{current_branch = }')
         
         status = git.get_status(repo)
 
@@ -24,61 +36,61 @@ def main():
                     case 'clean':
                         # TODO: parametrize origin and master
                         if args.local:
-                            print('getting local branches...')
+                            logger.info('getting local branches...')
                             local_branches = git.get_branches(repo, local=True, remote=False)
-                            print(f'{local_branches = }')
+                            logger.info(f'{local_branches = }')
                             for branch in local_branches:
                                 if branch in ('master', current_branch):
-                                    print(f'skipping {branch}...')
+                                    logger.info(f'skipping {branch}...')
                                 else:
                                     git.delete_branch(repo, branch, local=True, force=args.force)
                         else:
-                            print('skipping local branches...')
+                            logger.info('skipping local branches...')
                         
                         if args.remote:
-                            print('getting remote branches...')
+                            logger.info('getting remote branches...')
                             remote_branches = git.get_branches(repo, local=False, remote=True)
-                            print(f'{remote_branches = }')
+                            logger.info(f'{remote_branches = }')
                             for branch in remote_branches:
                                 branch = branch.removeprefix('origin/')
                                 if branch == 'master':
-                                    print('skipping master...')
+                                    logger.info('skipping master...')
                                 else:
                                     git.delete_branch(repo, branch, remote=True)
                         else:
-                            print('skipping remote branches...')
+                            logger.info('skipping remote branches...')
 
                     case 'updatemaster':
                         # TODO: change to update BRANCH, default=MASTER
                         # TODO: parametrize origin and master
                         
                         if status.dirty:
-                            print(f'ERROR | repo is dirty: commit or stash changes')
+                            logger.info(f'ERROR | repo is dirty: commit or stash changes')
                             sys.exit(1)
                         else:
-                            print(f'switching to master...')
+                            logger.info(f'switching to master...')
                             git.switch(repo, 'master')
-                            print(f'current branch: {git.get_current_branch(repo)}')
+                            logger.info(f'current branch: {git.get_current_branch(repo)}')
                             
-                            print(f'merging from {current_branch}...')
+                            logger.info(f'merging from {current_branch}...')
                             git.merge(repo, 'master', current_branch)
 
                             if args.push:
-                                print('pushing master...')
+                                logger.info('pushing master...')
                                 git.push(repo)
 
-                            print(f'switching back to {current_branch}...')
+                            logger.info(f'switching back to {current_branch}...')
                             git.switch(repo, current_branch)
-                            print(f'current branch: {git.get_current_branch(repo)}')
+                            logger.info(f'current branch: {git.get_current_branch(repo)}')
 
                     case 'foreach':
                         raise NotImplementedError()
 
     except git.GitNotARepoException:
-        print('please run this command inside a git repo')
+        logger.info('please run this command inside a git repo')
         sys.exit(1)
     except git.GitException as ge:
-        print(f'{ge.__module__}.{ge.__class__.__qualname__}: {ge}')
+        logger.info(f'{ge.__module__}.{ge.__class__.__qualname__}: {ge}')
         sys.exit(1)
     
     
