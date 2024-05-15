5caf2a9247b59c09cf4f96ad706998a8c6583095
atrox3d <atrox3d@gmail.com>
2024-05-15 10:47:04 +0200

    add debug from root logger

--- atrox3d/logging/logmanager.py
+++ atrox3d/logging/logmanager.py
@@ -10,8 +10,15 @@ class LoggingNotConfiguredException(Exception): pass
 _IS_LOGGING_CONFIGURED = False
 
 def is_logging_configured() -> bool:
-    ''' get the "private" value of flag '''
+    ''' "public" getter the "private" value of flag '''
+    logging.debug(f'getting value for {_IS_LOGGING_CONFIGURED = }')
     return _IS_LOGGING_CONFIGURED
+
+def _set_logging_configured(state: bool):
+    ''' "private setter for the value of the flag '''
+    global _IS_LOGGING_CONFIGURED
+    _IS_LOGGING_CONFIGURED = state
+    logging.debug(f'setting value for {_IS_LOGGING_CONFIGURED = }')
 
 def setup_logging(
                     root_level: int|str =logging.INFO,
@@ -30,8 +37,8 @@ def setup_logging(
     ]
 
     logging.basicConfig(level=root_level, format=format, handlers=handlers)
-    _IS_LOGGING_CONFIGURED = True
-    logging.info('logging configured')
+    _set_logging_configured(True)
+    logging.debug('logging configured')
 
 def get_logger(name: str, level: int|str =logging.INFO, configure=False) -> logging.Logger:
     ''' 
@@ -40,13 +47,15 @@ def get_logger(name: str, level: int|str =logging.INFO, configure=False) -> logg
     '''
     if not is_logging_configured():
         if not configure:
+            logging.error(f'logging has not been configured')
             raise LoggingNotConfiguredException('please call setuplogging to configure logging')
         else:
+            logging.debug('configuring default logging')
             setup_logging(level)
     
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    logging.info(f'logging already configured: obtaining logger {logger}')
+    logging.debug(f'logging is configured: obtaining logger {logger}')
     return logger
 
 def get_module_loggers(module: types.ModuleType) -> logging.Logger:
@@ -54,12 +63,14 @@ def get_module_loggers(module: types.ModuleType) -> logging.Logger:
     loggers = []
     for element in vars(module).values():
         if isinstance(element, logging.Logger):
+            logging.debug(f'adding {element} from {module}')
             loggers.append(element)
     return loggers
 
 def set_module_loggers_level(module: types.ModuleType, level: int|str):
     ''' sets all loggers for the specified module to level '''
     for logger in get_module_loggers(module):
+        logging.debug(f'setting level {logging.getLevelName(level)} for {logger}')
         logger.setLevel(level)
 
 def set_logger_level_for_modules(level: int|str, *modules: types.ModuleType):
@@ -74,6 +85,8 @@ def set_logger_level_for_imported_modules(level: int|str, name: str):
     set_logger_level_for_modules(level, *imported)
 
 if __name__ == '__main__':
+    logging.getLogger().setLevel('DEBUG')
+    
     print('import atrox3d.simplegit.git...')
     import atrox3d.simplegit.git
     print(f'{atrox3d.simplegit.git.logger = }')
