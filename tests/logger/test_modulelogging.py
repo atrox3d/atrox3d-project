36b2e4e8ab19b5ec8cc0f62014c133dabb561e7d
atrox3d <atrox3d@gmail.com>
2024-05-19 07:53:13 +0200

    add tests for module logging

--- tests/logger/test_modulelogging.py
+++ tests/logger/test_modulelogging.py
@@ -1,17 +1,26 @@
+import logging
 import unittest
-import sys
 
 import atrox3d.logger.modulelogging as ml
-import logging
-# from pathlib import Path
-
+from . import fakemodule
 
 class TestModuleLogging(unittest.TestCase):
 
+    def test_get_module_loggers(self):
+        loggers = ml.get_module_loggers(fakemodule)
+        self.assertEqual(1, len(loggers))
+        self.assertIsInstance(loggers[0], logging.Logger)
+
+    def test_set_module_loggers_level(self):
+        ml.set_module_loggers_level(fakemodule, 'ERROR')
+        for logger in ml.get_module_loggers(fakemodule):
+            self.assertEqual(logging.getLevelName(logger.getEffectiveLevel()), 'ERROR')
+
+    def test_set_logger_level_for_modules(self):
+        ml.set_logger_level_for_modules('ERROR', fakemodule)
+        for logger in ml.get_module_loggers(fakemodule):
+            self.assertEqual(logging.getLevelName(logger.getEffectiveLevel()), 'ERROR')
+
     def test_set_logger_level_for_imported_modules(self):
-        print('import atrox3d.simplegit.git...')
-        import atrox3d.simplegit.git
-        print(f'{atrox3d.simplegit.git.logger = }')
-        print(f'set level of loggers in imported modules to INFO... ')
         ml.set_logger_level_for_imported_modules('INFO', __name__)
-        print(f'{atrox3d.simplegit.git.logger = }')
+        print(f'{fakemodule.logger = }')
